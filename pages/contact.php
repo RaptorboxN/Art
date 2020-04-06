092930d7e8d0fe9bcb3ef4b9ecc543d7fd88da63
RaptorboxN <vanderburgnick@gmail.com>
2020-04-06 22:30:37 +0200

    Update contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,31 @@
 <?php
+?>
+
+<div class="ContactPage">
+    <div class="container">
+        <div class="text-center form">
+            <form class="ContactForm">
+                <div class="form-group">
+                    <label class="ContactLabel">{{__('contact.name')}}</label>
+                    <input class="ContactInput">
+                </div>
+                <div class="form-group">
+                    <label class="ContactLabel">{{__('contact.email')}} </label>
+                    <input class="ContactInput">
+                </div>
+                <div class="form-group">
+                    <label class="ContactLabel">{{__('contact.subject')}} </label>
+                    <input class="ContactInput">
+                </div>
+                <div class="form-group">
+                    <textarea class="ContactText" rows="5">
+
+                    </textarea>
+                </div>
+                <div class="form-group text-right">
+                    <input type="submit" value="submit" class="btn ContactBtn">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
