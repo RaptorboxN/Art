8ca063fe19e99cd50519f3770e63fa79c6164d11
RaptorboxN <vanderburgnick@gmail.com>
2020-04-07 02:51:11 +0200

    Update

--- pages/Courosel.php
+++ pages/Courosel.php
@@ -7,6 +7,7 @@ include '../nav.php';
 
 <div class="CouroselPage">
     <div class="CouroselMain">
+<!--        banner aan de boven kant-->
         <div id="carouselExampleIndicators" class="carousel slide align-content-center" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -15,22 +16,13 @@ include '../nav.php';
             </ol>
             <div class="carousel-inner align-content-center">
                 <div class="carousel-item active">
-                    <img class="d-block carouselItemImg" src="../IMG/Art1.jpg" alt="First slide">
-                    <p>
-
-                    </p>
+                    <img class="d-block carouselItemImg" src="../IMG/Art4.jpg" alt="First slide">
                 </div>
                 <div class="carousel-item">
                     <img class="d-block" src="../IMG/Art2.jpg" alt="Second slide">
-                    <p>
-
-                    </p>
                 </div>
                 <div class="carousel-item">
                     <img class="d-block" src="../IMG/Art3.jpg" alt="Third slide">
-                    <p>
-
-                    </p>
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -42,6 +34,27 @@ include '../nav.php';
                 <span class="sr-only">Next</span>
             </a>
         </div>
+<!--        einde van de banner-->
+<!--        begin van de inhoud van de pagina-->
+        <hr>
+        <div class="row carouselInfo">
+            <div class="col-3">
+                <img class="imgStyle" src="../IMG/Art4.jpg">
+            </div>
+            <div class="col-9">
+                <p>
+                    Alteration literature to or an sympathize mr imprudence.
+                    Of is ferrars subject as enjoyed or tedious cottage.
+                    Procuring as in resembled by in agreeable.
+                    Next long no gave mr eyes.
+                    Admiration advantages no he celebrated so pianoforte unreserved.
+                    Not its herself forming charmed amiable.
+                    Him why feebly expect future now.
+                </p>
+            </div>
+
+        </div>
+        <hr>
     </div>
 
 
