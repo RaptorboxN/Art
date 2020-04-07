9f17f870405f8b1f47c179656b0f5d8b4aa73733
RaptorboxN <vanderburgnick@gmail.com>
2020-04-07 02:13:09 +0200

    banner courosel werkt

--- pages/Courosel.php
+++ pages/Courosel.php
@@ -6,5 +6,52 @@ include '../nav.php';
 <link rel="stylesheet" type="text/css" href="../CSS/couroselStyle.css">
 
 <div class="CouroselPage">
+    <div class="CouroselMain">
+        <div id="carouselExampleIndicators" class="carousel slide align-content-center" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner align-content-center">
+                <div class="carousel-item active">
+                    <img class="d-block carouselItemImg" src="../IMG/Art1.jpg" alt="First slide">
+                    <p>
+
+                    </p>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block" src="../IMG/Art2.jpg" alt="Second slide">
+                    <p>
+
+                    </p>
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block" src="../IMG/Art3.jpg" alt="Third slide">
+                    <p>
+
+                    </p>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
+
 
 </div>
+
+
+
+posotion absolute
+right 0
+bottom 0
+left 0
+
+
