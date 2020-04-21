571fa573b92e7d707a1fc033ab0e62959b1395d1
DJURONIMO <djurn98@live.nl>
2020-04-21 09:45:52 +0200

    created 'tag aanmaken' front

--- tags.php
+++ tags.php
@@ -11,72 +11,94 @@ include 'resources/head.php';
 ?>
 
 <main role="main">
-
-  <div class="album py-5 bg-light">
     <div class="container">
-        <form action="resources/update.php" method="post">
-        <div class="row">
-            <div class="col-sm-5">
 
-                <select class="selectpicker float-right" data-style="btn-primary" title="Item" name="Item" required>
+        <hr>
 
-                      <?php
-                        include 'resources/db.php';
-                        $sql = "SELECT * FROM items";
-                        $result = $conn->query($sql);
-
-                        if ($result->num_rows > 0) {
-                        // output data of each row
-                        while($row = $result->fetch_assoc()) {
-                            echo '<option value="' . $row["id"] . '">' . $row["title"] . '</option>';
-                        }
-                        } else {
-                        echo "0 results";
-                        }
-                        $conn->close();
-                      ?>
-                  </select>
+        <form action="resources/test.php" method="post">
+            <div class="row">
+                <div class="col text-center"><h2>Tag aanmaken:</h2></div>
             </div>
+            <div class="row">
+                <div class="col-sm-3 offset-sm-4">
 
-            <div class="col-sm-2"></div>
+                    <input type="text" class="form-control" id="name" placeholder="Tag titel">
 
-            <div class="col-sm-5">
+                </div>
 
-                <select class="selectpicker" data-style="btn-danger" title="Tag" name="Tag" required>
-
-                      <?php
-                        include 'resources/db.php';
-                        $sql = "SELECT * FROM tags";
-                        $result = $conn->query($sql);
-
-                        if ($result->num_rows > 0) {
-                        // output data of each row
-                        while($row = $result->fetch_assoc()) {
-                            echo '<option value="' . $row["id"] . '">' . $row["title"] . '</option>';
-                        }
-                        } else {
-                        echo "0 results";
-                        }
-                        $conn->close();
-                      ?>
-                </select>
-                  </div>
-              </div>
-              <div class="row">
-                <div class="col-sm-4"></div>
-
-                <div class="col-sm-4 text-center">
+                <div class="col-sm-1 text-center">
                     <button class="btn btn-success text-center" type="submit" id="addButton" aria-haspopup="true" aria-expanded="false">
-                        Add
+                        Toevoegen
                     </button>
                 </div>
             </div>
         </form>
-      </div>
-    </div>
-</div>
-</div>
 
+        <hr>
+
+        <form action="resources/update.php" method="post">
+            <div class="row">
+                <div class="col text-center"><h2>Tag toewijzen:</h2></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-5">
+
+                    <select class="selectpicker float-right" title="Item" name="Item" required>
+
+                          <?php
+                            include 'resources/db.php';
+                            $sql = "SELECT * FROM items";
+                            $result = $conn->query($sql);
+
+                            if ($result->num_rows > 0) {
+                            // output data of each row
+                            while($row = $result->fetch_assoc()) {
+                                echo '<option value="' . $row["id"] . '">' . $row["title"] . '</option>';
+                            }
+                            } else {
+                            echo "0 results";
+                            }
+                            $conn->close();
+                          ?>
+                      </select>
+                </div>
+
+                <div class="col-sm-5 offset-2">
+
+                    <select class="selectpicker" title="Tag" name="Tag" required>
+
+                          <?php
+                            include 'resources/db.php';
+                            $sql = "SELECT * FROM tags";
+                            $result = $conn->query($sql);
+
+                            if ($result->num_rows > 0) {
+                            // output data of each row
+                            while($row = $result->fetch_assoc()) {
+                                echo '<option value="' . $row["id"] . '">' . $row["title"] . '</option>';
+                            }
+                            } else {
+                            echo "0 results";
+                            }
+                            $conn->close();
+                          ?>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4"></div>
+
+                <div class="col-sm-4 text-center">
+                    <button class="btn btn-success text-center" type="submit" id="addButton" aria-haspopup="true" aria-expanded="false">
+                        Toevoegen
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        <hr>
+
+    </div>
 </main>
 
 <?php
